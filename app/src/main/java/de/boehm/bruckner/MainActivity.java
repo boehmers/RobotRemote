799895d1503044bb2d5033ed4b4bf4d4ac3451e3
Manuel <manuelboehm1@t-online.de>
2015-06-17 16:06:24 +0200

    Design-Anpassungen

--- app/src/main/java/de/boehm/bruckner/MainActivity.java
+++ app/src/main/java/de/boehm/bruckner/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -78,7 +79,7 @@ public class MainActivity extends ActionBarActivity {
         forwardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(forwardButton.isChecked()) {
+                if (forwardButton.isChecked()) {
                     moveController.startMovingForward();
                 } else {
                     moveController.stopMovingForward();
@@ -151,7 +152,9 @@ public class MainActivity extends ActionBarActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                communicationController.sendMessage("TODO");
+                EditText text = (EditText) findViewById(R.id.editText);
+                Toast.makeText(getBaseContext(), text.getText().toString(), Toast.LENGTH_SHORT);
+                communicationController.sendMessage(text.getText().toString());
             }
         });
     }
